bb00522770d278b7bfe80713ed1a57d1c236db4e
Ajoa-Victor <victoroluwatimileyin3@gmail.com>
2026-07-24 17:39:40 +0100

    cross origin addition

--- aegis-sentinel-api/src/main.ts
+++ aegis-sentinel-api/src/main.ts
@@ -4,10 +4,16 @@ import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors(); // Crucial if you plan to attach a frontend UI
+  
+  app.enableCors({
+    origin: 'https://project-aegis-sentinel.vercel.app/', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  });
+  
   const port = process.env.PORT || 3000;
   
   await app.listen(port);
-  Logger.log(`Aegis Sentinel API is running on http://localhost:${port}`, 'Bootstrap');
+  Logger.log(`Aegis Sentinel API is running on port ${port}`, 'Bootstrap');
 }
 bootstrap();
